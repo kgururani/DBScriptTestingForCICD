ca6dc45c2a4d5785dd3a20b3c173adf2218ec7a1
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-07 16:46:24 +0530

    Create 1-02_insertUsers.sql

---
+++ DataBaseFiles/Version 1.x/1-02_insertUsers.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure1'a
+           ,'Testing1'
+           ,'kgururani@deloitte.com')
+GO
