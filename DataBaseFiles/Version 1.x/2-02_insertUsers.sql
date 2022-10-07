06fa2a30ca4a012300d939e52c3d2bd6ae4327eb
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-07 19:10:08 +0530

    Add files via upload

---
+++ DataBaseFiles/Version 1.x/2-02_insertUsers.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure3'
+           ,'Testing3'
+           ,'testing3@deloitte.com')
+GO
