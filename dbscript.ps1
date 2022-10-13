0de3419866ac6ed6cb762145c9cde6852e93d7fd
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-13 17:10:11 +0530

    Update dbscript.ps1

--- dbscript.ps1
+++ dbscript.ps1
@@ -89,55 +89,42 @@ function script-execute {
 	##Checking for table existence
 	$table_val= sqlcmd -h-1 -S $h -U $uname -P $password -v table= "$d.$table_name" -Q $query
 	if($table_val){
-		        write-host "PASS: Version table already exists in DB with table name $d.$table_name "
+		write-host "PASS: Version table already exists in DB with table name $d.$table_name "
 	}
 	else{
-		    Write-Warning " Version table does not exist in DB. Creating the table as $d.$table_name"
-		    $first_script= ($sql_file | Measure -Min).Minimum
-		    $first_script
-		    $first_script_target= Get-ChildItem "$repo_dir\DataBaseFiles\version-0\$first_script"
-			
-			##sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; CREATE TABLE $d.$table_name([CURRENT_VERSION] [nvarchar](250) NOT NULL,[SUB_VERSION] [nvarchar](250) NOT NULL) | Format-List | Out-String | ForEach-Object { $_.Trim() }
-			sqlcmd -S $h -U $uname -P $password -i $first_script_target
-
+	    Write-Warning " Version table does not exist in DB. Creating the table as $d.$table_name"
+	    $first_script= ($sql_file | Measure -Min).Minimum
+		$first_script
+		$first_script_target= Get-ChildItem "$repo_dir\DataBaseFiles\version-0\$first_script"
+		sqlcmd -S $h -U $uname -P $password -i $first_script_target
 	}
 
 	##fetch current version from database table
 	$db_version=sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; select CURRENT_VERSION from $d.$table_name" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 	$db_sub_version=sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; select SUB_VERSION from $d.$table_name" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 	
-	write-host "INFO: Version on Db: "$db_version
-	write-host "INFO: SUB Version on Db: "$db_sub_version
+	write-host "INFO: Current Version on Db: "$db_version
+	write-host "INFO: Current Sub Version on Db: "$db_sub_version
 	
 	for($i=0; $i -le ($sql_folders.length -1); $i +=1){
 		if($sql_folders[$i] -ne 'version-0'){
 			$version_num= $sql_folders[$i].split('-')[1]
-			write-host "FOLDER VERSION: " $version_num
 			$version_num_check= $version_num -match '\d{1,3}\.\d{1,3}\.\d{1,3}'
-			write-host "FOLDER VERSION: " $version_num_check
 			if($version_num_check -eq 'True'){
 				if($version_num -ge $db_version){
 					$sql_files= Split-Path -Path "$repo_dir\DataBaseFiles\version-$version_num\*.sql" -Leaf -Resolve
-					write-host "sql_files: " $sql_files
-
 					for($j=0; $j -le ($sql_files.length -1); $j +=1){
-						Write-Host "EXEC: executing script: "$sql_files[$j]
 						$sub_version_num= $sql_files[$j].split('-')[0]
-						write-host "TEST AGAIN: " $sub_version_num
 						$sub_version_num_check= $sub_version_num -match '\d{1,3}'
-						write-host "sub_version_num_check AGAIN: " $sub_version_num_check
-
 						if($sub_version_num_check -eq 'True'){
 							if($sub_version_num -gt $db_sub_version){
 								$exec_file=$sql_files[$j]
 								$target=Get-ChildItem "$repo_dir\DataBaseFiles\version-$version_num\$exec_file"
-								write-host "target: " $target
-
 								sqlcmd -S $h -U $uname -P $password -i $target
 								##Update current sub version from database table
 								sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$table_name" -Q "set nocount on; update $d.$table_name SET SUB_VERSION = '$sub_version_num'" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 								$db_updated_sub_version=sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; select SUB_VERSION from $d.$table_name" | Format-List | Out-String | ForEach-Object { $_.Trim() }
-								write-host "INFO: updated sub version_num: " $db_updated_sub_version
+								write-host "INFO: Updated Sub version_num: " $db_updated_sub_version
 							}
 						}
 						else {
@@ -150,7 +137,7 @@ function script-execute {
 					
 					sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$table_name" -Q "set nocount on; update $d.$table_name SET CURRENT_VERSION = '$version_num'" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 					$db_updated_version=sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; select CURRENT_VERSION from $d.$table_name" | Format-List | Out-String | ForEach-Object { $_.Trim() }
-					write-host "INFO: updated version_num: " $db_updated_version
+					write-host "INFO: Updated version_num: " $db_updated_version
 				}
 			}
 			else {
