cf4ff16e192f1ae8b8c33226666bbc4a76977b8d
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-13 17:13:40 +0530

    Add files via upload

---
+++ DataBaseFiles/version-2.8.9/1-MRMS_InsertUsers.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure1'
+           ,'Testing1'
+           ,'testing1@deloitte.com')
+GO
