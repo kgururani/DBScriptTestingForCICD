3da0e71b42b56163194ad77b2bca28043e0eff87
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-14 12:27:08 +0530

    Update dbscript.ps1

--- dbscript.ps1
+++ dbscript.ps1
@@ -106,7 +106,7 @@ function script-execute {
 	write-host "INFO: Current Version on Db: "$db_version
 	write-host "INFO: Current Sub Version on Db: "$db_sub_version
 	
-	for($i=0; $i -le ($sql_folders.length -1); $i +=1){
+	for($i=0; $i -le ($sql_folders.count -1); $i +=1){
 		write-host "INFO: LOOP:"$i
 		if($sql_folders[$i] -ne 'version-0'){
 			$version_num= $sql_folders[$i].split('-')[1]
@@ -122,7 +122,12 @@ function script-execute {
 						write-host "INFO:INSIDE LOOP:"$j
 						write-host "INFO: INSIDE LOOP FILES AGAIN:"$sql_files[$j]
 						
-						$sub_version_num= $sql_files[$j].split('-')[0]
+						if(sql_files.count -eq '1'){
+							$sub_version_num= $sql_files.split('-')[0]
+						}
+						else{
+							$sub_version_num= $sql_files[$j].split('-')[0]
+						}
 						$sub_version_num_check= $sub_version_num -match '\d{1,3}'
 						if($sub_version_num_check -eq 'True'){
 							if($version_num -gt $db_version){
