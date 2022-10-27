51baa5ca073e61772ee0c9d766cdbba83ca4035c
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-28 00:04:26 +0530

    Create 3-MRMS_insert.sql

---
+++ DataBaseFiles/version-2.6.9/3-MRMS_insert.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure02'
+           ,'Testing012'
+           ,'testing012@deloitte.com')
+GO
