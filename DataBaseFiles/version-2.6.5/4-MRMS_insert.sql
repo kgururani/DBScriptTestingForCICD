358a08d082cff658fd51f51c61f4d9590f2d7f22
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-27 14:17:19 +0530

    Create 4-MRMS_insert.sql

---
+++ DataBaseFiles/version-2.6.5/4-MRMS_insert.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure4'
+           ,'Testing4'
+           ,'testing4@deloitte.com')
+GO
