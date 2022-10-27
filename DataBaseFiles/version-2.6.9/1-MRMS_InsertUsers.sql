cb21123f8d75b1d46ba0f0cd64700f82069c21bb
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-27 14:13:07 +0530

    Create 1-MRMS_InsertUsers.sql

---
+++ DataBaseFiles/version-2.6.9/1-MRMS_InsertUsers.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure11'
+           ,'Testing11'
+           ,'testing11@deloitte.com')
+GO
