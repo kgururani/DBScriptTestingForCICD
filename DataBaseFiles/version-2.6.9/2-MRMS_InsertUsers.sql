6a4e2b134b5d67455da2838388846656f8fe2e3d
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-28 19:55:35 +0530

    Update 2-MRMS_InsertUsers.sql

--- DataBaseFiles/version-2.6.9/2-MRMS_InsertUsers.sql
+++ DataBaseFiles/version-2.6.9/2-MRMS_InsertUsers.sql
@@ -7,7 +7,7 @@ INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
            ,[EMAIL_ID])
 		   
      VALUES
-           ('Azure02'
-           ,'Testing012'
-           ,'testing012@deloitte.com')
+           ('Azure32'
+           ,'Testing32'
+           ,'testing032@deloitte.com')
 GO
