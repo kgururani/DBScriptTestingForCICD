f793e3da3d1a695bb874fc099a703c431704630d
kgururani <112697001+kgururani@users.noreply.github.com>
2022-10-31 19:12:55 +0530

    Create 6-Insert.sql

---
+++ DataBaseFiles/version-2.6.5/6-Insert.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure05'
+           ,'Testing05'
+           ,'testing05@deloitte.com')
+GO
