9a553688ee3b376251ea21c4203380ea23ab715a
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-02 00:08:38 +0530

    Create 9-kjnsdfhk.sql

---
+++ DataBaseFiles/version-2.6.5/9-kjnsdfhk.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure9'
+           ,'Testing5'
+           ,'testing5@deloitte.com')
+GO
