87377ffc17a201004b79b7f504e6d8e36969464d
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-02 00:07:25 +0530

    Update dbscript.ps1

--- dbscript.ps1
+++ dbscript.ps1
@@ -149,7 +149,7 @@ function script-execute {
 						}
 					}
 					#Check if version is already exist
-					$count_Rows = sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; SELECT COUNT(*) from $d.$version_table_logs WHERE VERSIONS = $db_version " | Format-List | Out-String | ForEach-Object { $_.Trim() }
+					$count_Rows = sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; SELECT COUNT(*) from $d.$version_table_logs WHERE VERSIONS = '$db_version' " | Format-List | Out-String | ForEach-Object { $_.Trim() }
 					if($count_Rows -eq '1'){
 						#UPDATE number of files executed from database table
 						sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$version_table_logs" -Q "set nocount on; UPDATE $d.$version_table_logs SET NUMBER_OF_FILES_EXECUTED = '$db_UPDATEd_sub_version' " | Format-List | Out-String | ForEach-Object { $_.Trim() }
@@ -161,8 +161,8 @@ function script-execute {
 					}
 
 					##UPDATE previous version from database table
-					sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$version_table" -Q "set nocount on; UPDATE $d.$version_table SET PREVIOUS_VERSION = '$version_num'" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 					$db_previous_version=sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; SELECT CURRENT_VERSION from $d.$version_table" | Format-List | Out-String | ForEach-Object { $_.Trim() }
+					sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$version_table" -Q "set nocount on; UPDATE $d.$version_table SET PREVIOUS_VERSION = '$db_previous_version'" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 					sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$version_table" -Q "set nocount on; UPDATE $d.$version_table SET MESSAGE = 'SUCCESS'" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 					exit 0
 				}
