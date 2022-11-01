ec5d73e4c942705144ba1b511d87e6820835c2fd
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-02 00:55:45 +0530

    Create 5-fdff.sql

---
+++ DataBaseFiles/version-2.6.9/5-fdff.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure55'
+           ,'Testing66'
+           ,'testing44@deloitte.com')
+GO
