05c96e57611b19a4204bdaad4977ce3022df0058
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-02 01:32:47 +0530

    Update dbscript.ps1

--- dbscript.ps1
+++ dbscript.ps1
@@ -131,8 +131,8 @@ function script-execute {
 						$sub_version_num_check= $sub_version_num -match '\d{1,3}'
 						if($sub_version_num_check){
 							write-host "INFO: 22 -- $sub_version_num" 
-							if($sub_version_num -gt $db_files_seq){
-							write-host "INFO: 33"
+							if([int]$sub_version_num -gt [int]$db_files_seq){
+								write-host "INFO: 33"
 								if($sql_files.count -eq '1'){
 									$exec_file=$sql_files
 								}
