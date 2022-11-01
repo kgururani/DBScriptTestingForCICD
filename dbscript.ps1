334fe21d0b4db2a6ef339e40f3d3779e9b7b2375
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-01 23:05:42 +0530

    Update dbscript.ps1

--- dbscript.ps1
+++ dbscript.ps1
@@ -140,10 +140,10 @@ function script-execute {
 	write-host "INFO: Current Version on DB: " $db_version
 	if($db_version -ne $db_previous_version){
 		#Check if version is already exist
-		$count_Rows = sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; SELECT COUNT(*) from $d.$version_table_logs WHERE VERSIONS = $db_version " | Format-List | Out-String | ForEach-Object { $_.Trim() }
+		$count_Rows = sqlcmd -h-1 -S $h -U $uname -P $password -Q "set nocount on; SELECT COUNT(*) from $d.$version_table_logs WHERE VERSIONS = $db_version" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 		write-host "INFO: count_Rows::::$count_Rows"
 
-		if(count_Rows -eq '1'){
+		if($count_Rows -eq '1'){
 			#UPDATE number of files executed from database table
 			$number_Of_Files_Executed =sqlcmd -h-1 -S $h -U $uname -P $password -v table = "$d.$version_table_logs" -Q "set nocount on; SELECT NUMBER_OF_FILES_EXECUTED from $d.$version_table_logs WHERE VERSIONS = $db_version "" | Format-List | Out-String | ForEach-Object { $_.Trim() }
 			write-host "INFO: number_Of_Files_Executed::::$number_Of_Files_Executed"
