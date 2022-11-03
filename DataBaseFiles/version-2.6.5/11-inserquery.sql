fb48846f21e845b5897bd8304bcdc1304ecaaae8
kgururani <112697001+kgururani@users.noreply.github.com>
2022-11-03 16:29:21 +0530

    Create 11-inserquery.sql

---
+++ DataBaseFiles/version-2.6.5/11-inserquery.sql
@@ -0,0 +1,13 @@
+USE [BACMRMQA]
+GO
+
+INSERT INTO [dbo].[PIPELINE_CICD_TESTING]
+           ([FIRST_NAME]
+           ,[LAST_NAME]
+           ,[EMAIL_ID])
+		   
+     VALUES
+           ('Azure6'
+           ,'Testing6'
+           ,'testing6@deloitte.com')
+GO
